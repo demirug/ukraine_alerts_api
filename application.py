924cab96a21327749533b46920de2c1e173a92d6
demirug <pm.antondolia@gmail.com>
2023-02-11 11:02:43 +0200

    APP refactor

--- application.py
+++ application.py
@@ -8,14 +8,24 @@ dotenv.load_dotenv()
 
 db = SQLAlchemy()
 
-app = Flask(__name__)
-app.config.from_object(Config)
 
-db.init_app(app)
+def create_app():
+    app = create_base_app()
 
-from models import *
+    from app.controller import main
+    app.register_blueprint(main, url_prefix="")
 
-with app.app_context():
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
-from routes import *
+    return app
+
+
+def create_base_app():
+    global app
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    return app
