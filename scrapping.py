d008733d0df3ed3b73bd4af78cb1c5bbaec6fd9e
demirug <pm.antondolia@gmail.com>
2023-02-11 13:17:28 +0200

    Updated scrapping

--- scrapping.py
+++ scrapping.py
@@ -7,11 +7,16 @@ from selenium.webdriver.common.by import By
 def get_alert_data(url="https://alerts.in.ua"):
     driver = webdriver.Chrome()
     driver.get(url)
-    time.sleep(1)
+    time.sleep(3)  # Time to load JS script 100%
     content = driver.find_elements(By.CSS_SELECTOR, "#map > svg > g.text-labels > text.map-label")
+    driver.close()
     data = list()
 
     for el in set(content):
-        data.append({"name": el.get_attribute('aria-label'), "alert": 'active' in el.get_attribute('class').split(' ')})
+        data.append({
+            "name": el.get_attribute('aria-label'),
+            "alert": 'active' in el.get_attribute('class').split(' '),
+            "is_city": el.get_attribute('aria-label').startswith('м.')
+        })
 
     return data
