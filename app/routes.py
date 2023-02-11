7e63240b26d1433113c1bf5e1441d86e6625aeda
demirug <pm.antondolia@gmail.com>
2023-02-11 19:31:21 +0200

    Created API view

---
+++ app/routes.py
@@ -0,0 +1,54 @@
+from flask_restx import Resource
+from sqlalchemy import func, and_
+
+from app.models import Region, RegionStatus
+from app.schemas import RegionSchema, RegionStatusSchema
+from application import db, api
+
+
+@api.route('/regions')
+class RegionList(Resource):
+
+    def get(self):
+        """
+        Return: json list of all registered regions
+        Fields: id, name, is_city
+        """
+
+        return RegionSchema(many=True).dump(Region.query.all())
+
+
+@api.route('/status')
+class RegionStatusList(Resource):
+    def get(self):
+        """
+        Return: json list of all current region statuses
+        Fields: region_id, is_alert, timestamp
+
+        Using SQL Query to get regions statuses
+
+        SELECT r.* FROM
+            (SELECT region_id, MAX(TIMESTAMP) AS timestamp FROM regions_status GROUP BY region_id) c
+        JOIN regions_status r ON r.region_id = c.region_id and r.timestamp = c.timestamp
+        """
+
+        stmt = db.session.query(RegionStatus.region_id.label('region_id'),
+                                func.MAX(RegionStatus.timestamp).label('timestamp')).group_by(
+            RegionStatus.region_id).subquery()
+
+        regions = RegionStatus.query.join(stmt, and_(RegionStatus.region_id == stmt.c.region_id,
+                                                     RegionStatus.timestamp == stmt.c.timestamp)).all()
+
+        return RegionStatusSchema(many=True).dump(regions)
+
+
+@api.route('/status/<int:id>')
+class RegionStatusList(Resource):
+    def get(self, id):
+        """
+        Return: json of region current status
+        Fields: region_id, is_alert, timestamp
+        """
+
+        region_status: RegionStatus = RegionStatus.query.filter(RegionStatus.region_id==id).order_by(RegionStatus.timestamp.desc()).first()
+        return RegionStatusSchema().dump(region_status)
