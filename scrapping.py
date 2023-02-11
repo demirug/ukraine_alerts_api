13d71f26a6025ef5aee7775a75d89fdc05ad591f
demirug <pm.antondolia@gmail.com>
2023-02-11 21:47:28 +0200

    Added driver arguments

--- scrapping.py
+++ scrapping.py
@@ -5,11 +5,24 @@ from selenium.webdriver.common.by import By
 
 
 def get_alert_data(url="https://alerts.in.ua"):
-    driver = webdriver.Chrome()
+
+    options = webdriver.ChromeOptions()
+
+    options.add_argument('start-maximized')
+    options.add_argument('disable-infobars')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--remote-debugging-port=9222')
+    options.add_argument('--headless')
+
+    driver = webdriver.Chrome(options=options)
     driver.get(url)
     time.sleep(3)  # Time to load JS script 100%
     content = driver.find_elements(By.CSS_SELECTOR, "#map > svg > g.text-labels > text.map-label")
-    driver.close()
+
+
     data = list()
 
     for el in set(content):
@@ -18,5 +31,7 @@ def get_alert_data(url="https://alerts.in.ua"):
             "alert": 'active' in el.get_attribute('class').split(' '),
             "is_city": el.get_attribute('aria-label').startswith('м.')
         })
+
+    driver.close()
 
     return data
